d47bb81869fcc8933603632103b0e387cfe08d38
RafaelCapo <48961695+RafaelCapo@users.noreply.github.com>
2019-04-17 19:45:52 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+require_once 'app/Core/Core.php';
+
+require_once 'app/Controller/HomeController.php';
+require_once 'app/Controller/ErroController.php';
+
+
+
+$template = file_get_contents('app/Template/estrutura.html');
+
+ob_start();
+	$core = new Core;
+	$core->start($_GET);
+
+	$saida = ob_get_contents();
+ob_end_clean();
+
+$tplPronto = str_replace('{{area_dinamica}}', $saida, $template);
+echo $tplPronto;
