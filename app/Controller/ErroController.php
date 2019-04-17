573e571b8f5eabd50c4349adf20d3d84e5cc476f
RafaelCapo <48961695+RafaelCapo@users.noreply.github.com>
2019-04-17 19:44:54 -0300

    Add files via upload

---
+++ app/Controller/ErroController.php
@@ -0,0 +1,9 @@
+<?php
+
+	class ErroController
+	{
+		public function index()
+		{
+			echo 'pagina de erro';
+		}
+	}
---
+++ app/Controller/HomeController.php
@@ -0,0 +1,9 @@
+<?php
+
+	class HomeController
+	{
+		public function index()
+		{
+			echo 'teste';
+		}
+	}
