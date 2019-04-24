682a736cd349fc798fe2ac50ff20d7d1f69a1964
RafaelCapo <48961695+RafaelCapo@users.noreply.github.com>
2019-04-24 20:05:16 -0300

    Add files via upload

---
+++ app/Model/Postagem.php
@@ -0,0 +1,27 @@
+<?php
+
+	class Postagem
+	{
+
+		public static function selecionaTodos()
+		{
+			$con = Connection::getConn();
+
+			$sql = "SELECT * FROM postagem ORDER BY id DESC";
+			$sql = $con->prepare($sql);
+			$sql->execute();
+
+			$resultado = array();
+
+			while ($row = $sql->fetchObject('Postagem')) {
+				$resultado[] = $row;
+			}
+
+			if (!$resultado) {
+				throw new Exception("Não foi encontrado nenhum registro no banco");		
+			}
+
+			return $resultado;
+		}
+
+	}
