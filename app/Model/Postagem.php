761220811bb33b8bdb5c7f9f0bd58a3ff2e134ad
RafaelCapo <48961695+RafaelCapo@users.noreply.github.com>
2019-05-08 21:30:02 -0300

    Add files via upload

--- app/Model/Postagem.php
+++ app/Model/Postagem.php
@@ -24,4 +24,22 @@
 			return $resultado;
 		}
 
+		public static function selecionaPorId($idPost)
+		{
+			$con = Connection::getConn();
+
+			$sql = "SELECT * FROM postagem WHERE id = :id";
+			$sql = $con->prepare($sql);
+			$sql->bindValue(':id', $idPost, PDO::PARAM_INT);
+			$sql->execute();
+
+			$resultado = $sql->fetchObject('Postagem');
+
+			if (!$resultado) {
+				throw new Exception("Não foi encontrado nenhum registro no banco");	
+			}
+
+			return $resultado;
+		}
+
 	}
