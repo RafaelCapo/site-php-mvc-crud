c17903a4c12161f807efd0c123dbe71044997367
RafaelCapo <48961695+RafaelCapo@users.noreply.github.com>
2019-05-29 23:10:59 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -8,6 +8,7 @@ require_once 'app/Controller/HomeController.php';
 require_once 'app/Controller/ErroController.php';
 require_once 'app/Controller/PostController.php';
 require_once 'app/Controller/SobreController.php';
+require_once 'app/Controller/AdminController.php';
 
 require_once 'app/Model/Postagem.php';
 require_once 'app/Model/Comentario.php';
