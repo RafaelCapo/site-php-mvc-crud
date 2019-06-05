c774bcd5db1888c02f3675aa97647009073b8bc4
RafaelCapo <48961695+RafaelCapo@users.noreply.github.com>
2019-06-05 17:30:27 -0300

    Add files via upload

--- app/Controller/AdminController.php
+++ app/Controller/AdminController.php
@@ -8,7 +8,10 @@
 			$twig = new \Twig\Environment($loader);
 			$template = $twig->load('admin.html');
 
+			$objPostagens = Postagem::selecionaTodos();
+
 			$parametros = array();
+			$parametros['postagens'] = $objPostagens;
 
 			$conteudo = $template->render($parametros);
 			echo $conteudo;
@@ -28,6 +31,15 @@
 
 		public function insert()
 		{
-			var_dump($_POST);
+			try {
+				Postagem::insert($_POST);
+
+				echo '<script>alert("Publicação inserida com sucesso!");</script>';
+				echo '<script>location.href="http://localhost/PROJS/VIDEO_AULAS/SERIE/02_PHP+MVC+CRUD/?pagina=admin&metodo=index"</script>';
+			} catch(Exception $e) {
+				echo '<script>alert("'.$e->getMessage().'");</script>';
+				echo '<script>location.href="http://localhost/PROJS/VIDEO_AULAS/SERIE/02_PHP+MVC+CRUD/?pagina=admin&metodo=create"</script>';
+			}
+			
 		}
 	}
