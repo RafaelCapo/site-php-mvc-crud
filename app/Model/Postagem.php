46f0a17e50db72027a70120feed06b08e5cbf7ae
RafaelCapo <48961695+RafaelCapo@users.noreply.github.com>
2019-06-15 08:25:26 -0300

    Add files via upload

--- app/Model/Postagem.php
+++ app/Model/Postagem.php
@@ -68,4 +68,24 @@
 			return true;
 		}
 
+		public static function update($params)
+		{
+			$con = Connection::getConn();
+
+			$sql = "UPDATE postagem SET titsulo = :tit, conteudo = :cont WHERE id = :id";
+			$sql = $con->prepare($sql);
+			$sql->bindValue(':tit', $params['titulo']);
+			$sql->bindValue(':cont', $params['conteudo']);
+			$sql->bindValue(':id', $params['id']);
+			$resultado = $sql->execute();
+
+			if ($resultado == 0) {
+				throw new Exception("Falha ao alterar publicação");
+
+				return false;
+			}
+
+			return true;
+		}
+
 	}
