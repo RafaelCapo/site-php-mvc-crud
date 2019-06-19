e27f6dc1757c4904c8baeded4b0c23237b0c3b5b
RafaelCapo <48961695+RafaelCapo@users.noreply.github.com>
2019-06-19 16:53:26 -0300

    Add files via upload

--- app/Model/Postagem.php
+++ app/Model/Postagem.php
@@ -88,4 +88,22 @@
 			return true;
 		}
 
+		public static function delete($id)
+		{
+			$con = Connection::getConn();
+
+			$sql = "DELETE FROM postagem WHERE id = :id";
+			$sql = $con->prepare($sql);
+			$sql->bindValue(':id', $id);
+			$resultado = $sql->execute();
+
+			if ($resultado == 0) {
+				throw new Exception("Falha ao deletar publicação");
+
+				return false;
+			}
+
+			return true;
+		}
+
 	}
